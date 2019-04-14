26583ce5fc3acc3ef6d9111b276ff718088ee887
Danilo Moreira <danilo.moreira@fxdata.com.br>
2019-04-14 17:19:22 -0300

    v5

--- resources/hotel.py
+++ resources/hotel.py
@@ -29,57 +29,37 @@ class Hoteis(Resource):
         return {'hoteis': hoteis}
 
 class Hotel(Resource):
-    def get(self, hotel_id):
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('nome')
+    atributos.add_argument('estrelas')
+    atributos.add_argument('diaria')
+    atributos.add_argument('cidade')
+
+    def find_hotel(hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return False
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
         return {'message': 'Hotel not found.'}, 404
 
     def post(self, hotel_id):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('nome')
-        atributos.add_argument('estrelas')
-        atributos.add_argument('diaria')
-        atributos.add_argument('cidade')
-
-        dados = atributos.parse_args()
-
-        novo_hotel = {
-                    'hotel_id': hotel_id,
-                    'nome': dados['nome'],
-                    'estrelas': dados['estrelas'],
-                    'diaria': dados['diaria'],
-                    'cidade': dados['cidade']
-                     }
         hoteis.append(novo_hotel)
         return novo_hotel, 201
 
     def put(self, hotel_id):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('nome')
-        atributos.add_argument('estrelas')
-        atributos.add_argument('diaria')
-        atributos.add_argument('cidade')
-        dados = atributos.parse_args()
-        novo_hotel = {
-                    'hotel_id': hotel_id,
-                    'nome': dados['nome'],
-                    'estrelas': dados['estrelas'],
-                    'diaria': dados['diaria'],
-                    'cidade': dados['cidade']
-                     }
-
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                hotel_encontrado = hotel
-            else:
-                hotel_encontrado = None
-
-        if hotel_encontrado:
-            hotel_encontrado.update(novo_hotel)
-            return hotel_encontrado
+        dados = Hotel.atributos.parse_args()
+        novo_hotel =    {'hotel_id': hotel_id, **dados}
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return hotel, 200
         hoteis.append(novo_hotel)
-        return novo_hotel
+        return novo_hotel, 201
 
     def delete(self, hotel_id):
         global hoteis
