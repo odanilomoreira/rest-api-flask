1d46cac7625234f5ac1e18ab77e48c34ce29b8ce
Danilo Moreira <danilo.moreira@fxdata.com.br>
2019-04-14 17:14:37 -0300

    adjustments

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ from resources.hotel import Hoteis
 app = Flask(__name__)
 api = Api(app)
 
-class Hoteis(Resource):
-    def get(self):
-        return {'hoteis': 'meus hoteis'}
-
 api.add_resource(Hoteis, '/hoteis')
 
 if __name__ == '__main__':
