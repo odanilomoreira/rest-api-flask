c65b5ffd91513e17fce5221a3d083a2514436034
Danilo Moreira <danilo.moreira@fxdata.com.br>
2019-04-15 12:22:33 -0300

    Adicionando BD via py script

--- resources/hotel.py
+++ resources/hotel.py
@@ -50,8 +50,6 @@ class Hotel(Resource):
 
     def post(self, hotel_id):
         dados = Hotel.atributos.parse_args()
-
-        # novo_hotel = {'hotel_id': hotel_id, **dados}
         novo_hotel = HotelModel(hotel_id, **dados) # Objeto
 
         hoteis.append(novo_hotel.json())
@@ -59,7 +57,6 @@ class Hotel(Resource):
 
     def put(self, hotel_id):
         dados = Hotel.atributos.parse_args()
-        # novo_hotel =    {'hotel_id': hotel_id, **dados}
         novo_hotel = HotelModel(hotel_id, **dados)
 
         hotel = Hotel.find_hotel(hotel_id)
