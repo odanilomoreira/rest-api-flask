a166b06c2856daab809206035aafbae06db8d776
Danilo Moreira <danilo.moreira@fxdata.com.br>
2019-04-15 07:27:40 -0300

    Adicionando HotelModel

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
         {
@@ -49,19 +50,24 @@ class Hotel(Resource):
 
     def post(self, hotel_id):
         dados = Hotel.atributos.parse_args()
-        novo_hotel = {'hotel_id': hotel_id, **dados}
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201
+
+        # novo_hotel = {'hotel_id': hotel_id, **dados}
+        novo_hotel = HotelModel(hotel_id, **dados) # Objeto
+
+        hoteis.append(novo_hotel.json())
+        return novo_hotel.json(), 201
 
     def put(self, hotel_id):
         dados = Hotel.atributos.parse_args()
-        novo_hotel =    {'hotel_id': hotel_id, **dados}
+        # novo_hotel =    {'hotel_id': hotel_id, **dados}
+        novo_hotel = HotelModel(hotel_id, **dados)
+
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
-            hotel.update(novo_hotel)
+            hotel.update(novo_hotel.json())
             return hotel, 200
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201
+        hoteis.append(novo_hotel.json())
+        return novo_hotel.json(), 201
 
     def delete(self, hotel_id):
         global hoteis
