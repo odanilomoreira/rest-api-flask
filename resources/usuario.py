82a036ffb25581e8e95a009d057528c94e3b709d
Danilo Moreira <danilo.moreira@fxdata.com.br>
2019-04-17 08:52:42 -0300

    Fazendo login e exigindo login pra executar operacoes de alteracao no banco

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,5 +1,13 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token, jwt_required
+from werkzeug.security import safe_str_cmp
+
+#pip install Flask-JWT-Extended
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank.")
+atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank.")
 
 class User(Resource):
 
@@ -9,6 +17,7 @@ class User(Resource):
             return user.json()
         return {'message': 'User not found.'}, 404
 
+    @jwt_required
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
@@ -18,16 +27,27 @@ class User(Resource):
 
 class UserRegister(Resource):
     # /cadastro
-
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank.")
-        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank.")
+
         dados = atributos.parse_args()
 
         if UserModel.find_by_login(dados['login']):
-            return {"message": "The login '{}' already exists.".format(data['login'])}, 400 # bad request
+            return {"message": "The login '{}' already exists.".format(dados['login'])}, 400 # bad request
 
         user = UserModel(**dados)
         user.save_user()
         return {'message': 'User created successfully.'}, 201 # created
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+
+        user = UserModel.find_by_login(dados['login'])
+
+        if user and safe_str_cmp(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {'access_token': token_de_acesso}, 200
+
+        return {'message': 'The username or password is incorrect.'}, 401 # unauthorized
